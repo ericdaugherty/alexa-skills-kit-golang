c82b07e830acf54b277e82346a2865ef1f44c338
Eric Daugherty <eric@ericdaugherty.com>
2018-03-12 15:59:41 -0600

    Changed IntentSlot.ID to string from pointer. With omitempty, pointer unneccessary.

--- alexa.go
+++ alexa.go
@@ -77,10 +77,10 @@ type Intent struct {
 
 // IntentSlot contains the data for one Slot
 type IntentSlot struct {
-	Name               string  `json:"name"`
-	ConfirmationStatus string  `json:"confirmationStatus,omitempty"`
-	Value              string  `json:"value"`
-	ID                 *string `json:"id,omitempty"`
+	Name               string `json:"name"`
+	ConfirmationStatus string `json:"confirmationStatus,omitempty"`
+	Value              string `json:"value"`
+	ID                 string `json:"id,omitempty"`
 }
 
 // ResponseEnvelope contains the Response and additional attributes.

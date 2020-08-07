31b9a76d9d1175e1ee7d59ebf05af83b579a167c
Eric Klaus <etklaus@gmail.com>
2020-08-06 21:09:28 -0500

    Mark IntentSlot.SlotValue as beta

--- alexa.go
+++ alexa.go
@@ -112,24 +112,24 @@ type Intent struct {
 
 // IntentSlot contains the data for one Slot
 type IntentSlot struct {
-	Name               string `json:"name"`
-	ConfirmationStatus string `json:"confirmationStatus,omitempty"`
-	SlotValue   *IntentSlotValue `json:"slotValue"`
+	Name               string       `json:"name"`
+	ConfirmationStatus string       `json:"confirmationStatus,omitempty"`
+	Value              string       `json:"value"`
+	Resolutions        *Resolutions `json:"resolutions,omitempty"`
 
-	// Deprecated: use SlotValue.Value instead
-	Value string `json:"value"`
-	// Deprecated: use SlotValue.Resolutions instead
-	Resolutions *Resolutions     `json:"resolutions,omitempty"`
+	// SlotValue is a BETA field and may be removed by Amazon without warning.
+	// See https://developer.amazon.com/en-US/docs/alexa/custom-skills/collect-multiple-values-in-a-slot.html.
+	SlotValue *IntentSlotValue `json:"slotValue"`
 }
 
 // IntentSlotValue contains the value or values of a slot.
 // When Type == "Simple", Value and Resolutions are populated.
 // When Type == "List", Values is populated.
 type IntentSlotValue struct {
-	Type string `json:"type"`
-	Values []*IntentSlotValue `json:"values"`
-	Value string `json:"value"`
-	Resolutions *Resolutions `json:"resolutions,omitempty"`
+	Type        string             `json:"type"`
+	Values      []*IntentSlotValue `json:"values"`
+	Value       string             `json:"value"`
+	Resolutions *Resolutions       `json:"resolutions,omitempty"`
 }
 
 // Resolutions contain the (optional) ID of a slot
